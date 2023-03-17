eef8f1e33ef14825278ea69c138ec848caf97a6a
LukaArambasic01 <lukaarambasic01@gmail.com>
2023-03-18 00:17:13 +0100

    First

---
+++ settings.py
@@ -0,0 +1,28 @@
+# Display settings
+DEFAULT_IMAGE_SIZE = (300, 300)
+FPS = 120
+HEIGHT = 1000
+WIDTH = 1600
+START_X, START_Y = 0, -300
+X_OFFSET, Y_OFFSET = 20, 0
+
+# Images
+BG_IMAGE_PATH = 'graphics/art/matrix.png'
+GRID_IMAGE_PATH = 'graphics/art/gridline.png'
+GAME_INDICES = [1, 2, 3] 
+SYM_PATH = 'graphics/art/symbols'
+
+# Text
+TEXT_COLOR = 'White'
+UI_FONT = 'graphics/font/CasinoShadow.ttf'
+UI_FONT_SIZE = 30
+WIN_FONT_SIZE = 110
+
+# Symbol tokens
+symbols = {
+    'neo': f"{SYM_PATH}/neo.png", 
+    'morpheus': f"{SYM_PATH}/morpheus.png",
+    'trinity': f"{SYM_PATH}/trinity.png",
+    'smith': f"{SYM_PATH}/smith.png",
+    'sentinel': f"{SYM_PATH}/sentinel.png"
+}
